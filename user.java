d9c343544605ae5b2c5e469e585faca9526eacf7
marialuizaraposo <marialuizatavaresraposo@gmail.com>
2025-03-19 20:07:55 -0300

    Add files via upload

---
+++ user.java
@@ -0,0 +1,19 @@
+@getters
+@setters
+public class user {
+    private int id;
+    private String username;
+    private string password;
+    private string email;
+    private date birthday;
+    private string;
+
+    public user(String newname, String newpassword, String newemail) {
+        this.username = newName;
+        this.email = newEmail
+        this.password = newpassword;
+
+        public void Update Birthday( Date newBirthDate) {
+        this.birthday = newBirthDate;
+    }
+}
